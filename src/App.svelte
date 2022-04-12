a770e351522f204c7486fb56e5555d3a5977c637
Nipinium <nipinium@gmail.com>
2022-04-13 01:04:36 +0700

    remove unneeded permissions

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,27 @@
-<script>
-	export let name;
-</script>
-
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Chivi MTL</h1>
+  <p>Chivi là máy dịch tự động dịch tiếng Trung sang tiếng Việt</p>
+  <p>Visit <a href="https://chivi.app">Chivi</a> để biết thêm chi tiết</p>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
